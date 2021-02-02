b4dedfeb319ab9ceac1699fc5d4a7706091b9536
Connor Burns <connor.burns19@gmail.com>
2021-02-02 18:23:20 -0500

    Lab 3

---
+++ lab3/src/components/homepage/index.js
@@ -0,0 +1,21 @@
+/**
+ * Add a new component, HomePage that extends React.Component.
+HomePage should display the property firstName as part of its render() function using JSX
+ The "App.js" file should:
+Not contain the default text or styles
+Import HomePage
+Pass as an attribute firstName with your first name
+ * 
+ */
+
+import React from 'react';
+
+class HomePage extends React.Component {
+    render(firstName){
+        return(
+            firstName = <p>Connor</p>
+        );
+    }
+}
+
+export default HomePage;
